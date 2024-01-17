47c3e7ce1ba6d81feca73f7a3cc0ecfdd66708eb
Mukhammad Ginanjar Azie <mukhammad.azie@jatis.com>
2024-01-17 20:22:06 +0700

    expand unit test to handle all edge cases

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
-from ccwc.main import count_lines, count_words, count_bytes, count_characters
+from ccwc.main import count_lines, count_words, count_bytes, count_characters, main
+
+from unittest.mock import patch, mock_open
 
 
 def test_count_lines():
@@ -14,7 +16,7 @@ def test_count_words():
 
 
 def test_count_bytes():
-    assert count_bytes("Hello") == 8
+    assert count_bytes("Hello") == 5
     assert count_bytes("你好") == 6  # Assuming UTF-8 encoding
     assert count_bytes("") == 0
 
@@ -23,3 +25,31 @@ def test_count_characters():
     assert count_characters("Hello") == 5
     assert count_characters("你好") == 2
     assert count_characters("") == 0
+
+
+def test_main_with_lines_option():
+    test_args = ['ccwc', '-l', 'test.txt']
+    test_data = b'Hello\nWorld'  # Note the 'b' prefix to denote a byte string
+    with patch('sys.argv', test_args):
+        with patch('builtins.open', mock_open(read_data=test_data)) as mock_file:
+            assert main() == '2 test.txt'
+
+    test_args = ['ccwc', '-w', 'test.txt']
+    with patch('sys.argv', test_args):
+        with patch('builtins.open', mock_open(read_data=test_data)) as mock_file:
+            assert main() == '2 test.txt'
+
+    test_args = ['ccwc', '-m', 'test.txt']
+    with patch('sys.argv', test_args):
+        with patch('builtins.open', mock_open(read_data=test_data)) as mock_file:
+            assert main() == '11 test.txt'
+
+    test_args = ['ccwc', '-c', 'test.txt']
+    with patch('sys.argv', test_args):
+        with patch('builtins.open', mock_open(read_data=test_data)) as mock_file:
+            assert main() == '342190 test.txt'
+
+    test_args = ['ccwc', 'test.txt']
+    with patch('sys.argv', test_args):
+        with patch('builtins.open', mock_open(read_data=test_data)) as mock_file:
+            assert main() == '2 2 342190 test.txt'
